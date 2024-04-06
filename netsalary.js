774086aae5df5953e3535e4a907699a631fe3da1
nathansebby <146984952+nathansebby@users.noreply.github.com>
2024-04-06 08:29:07 +0300

    Update netsalary.js

--- netsalary.js
+++ netsalary.js
@@ -1,24 +1,30 @@
-Function netpay =0.3;
- const tax rate =o.3
- const nhif rate =0.02;
- const nssfRate =0.01;
+function netPay(){
+  const taxRate = 0.3;
+  const nhifRate = 0.02;
+  const nssfRate = 0.01;
 
- const basicSalary= parseFloat(prompt('enter your basic salary:'));
- const allowance =parseFloat(prompt(prompt("Enter your total allowance")));
-
- const grosssalary = basicSalary+allowance
-
- const nssfDeduction =Math.min(grosssalary*nssfRate);
- const nssfDeduction =Math.min(grosssalary*nhifRate);
- const paye =Math.max(grosssalary-nhifDeduction-paye;
   
+  const basicSalary = parseFloat(prompt("Enter your basic salary: "));
+  const allowances = parseFloat(prompt("Enter your total allowances: "));
+
+  
+  const grossSalary = basicSalary + allowances;
+
+  
+
+  const nssfDeduction = Math.min(grossSalary * nssfRate);
+  const nhifDeduction = Math.min(grossSalary * nhifRate);
+  const paye = Math.max(grossSalary - nhifDeduction - nssfDeduction) * taxRate
+
+ 
+  const netSalary = grossSalary - nhifDeduction - nssfDeduction - paye;
+
   
   console.log(window.alert(`NHIF deduction is: ${nhifDeduction}`));
-  console.log(window.alert(`NHIF deduction is: ${nssfDeduction}`));
-
-  console.log(window.alert(`PAYE: ${PAYE}`));
-  console.log(window.alert(`The gross salary is: ${grosssalary}`));
-  console.log(window.alert(`The net pay: ${nhifDeduction}`));
-  )
-  netpay();
+  console.log(window.alert(`NSSF deduction: ${nssfDeduction}`));
+  console.log(window.alert(`PAYE: ${paye}`));
+  console.log(window.alert(`The gross salary is: ${grossSalary}`));
+  console.log(window.alert(`The net pay: ${netSalary}`)); 
+}
+netPay(4000);
   
