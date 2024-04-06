f0d79a80a737de236b0856ec1b125f1591bb12bb
nathansebby <146984952+nathansebby@users.noreply.github.com>
2024-04-06 08:28:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-function calculateGrade(mark){
-  if (mark > 79){
-    return 'A';
-  }else if (mark >= 60){
-      return 'B'
-  }else if (mark >= 50){
-      return 'C'
-      }else (mark< 40) 
-      return "E"
-      }
+function studentgrades(marks){
+  if (marks>=0 && marks<=100) {
+    if(marks>79){
+      console.log(`A`)
+    }
+    else if(marks>=60 && marks <=79)[
+      console.log(`B`)
+    ]
+    else if(marks>=49 && marks <=59)[
+      console.log(`C`)
+    ]
+    else if(marks>=40 && marks <=49)[
+      console.log(`D`)
+    ]
+    else if( marks <40)[
+      console.log(`E`)
+    ]
+
+
+
+  } else {
+    console.log(`input marks in between 0 and 100`);
     
-
-
-function main() {
-  if(true){
-    let input= prompt("ENTER THE STUDENT Marks(between 0 and 100);"
-    let mark = parseFloat(input);
-
-    if(Boolean(mark) && mark >= 0 && mark <= 100){
-      let grade= calculateGrade(mark);
-      alert(`The grade for ${mark}is${grade}`);
-    }
-    else{
-      alert("please enter a valid mark between 0 and 100.");
-    }
   }
 }
-main();
+studentgrades(58)
